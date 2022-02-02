b9ac84d13d608db90ddb03cab91ac94d8f2bfbe8
Pierre <pierre.lagadec@gmail.com>
2022-02-02 13:01:18 +1100

    set up webgl

---
+++ index.js
@@ -0,0 +1,15 @@
+main();
+
+function main()
+{
+    const canvas = document.querySelector('#glcanvas');
+    const gl = canvas.getContext('webgl');
+
+    if(!gl) {
+        alert('Unable to setup WebGL. Your browser mayb not support it!');
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
