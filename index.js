526702537681a74c21d95a7e3fd39387f6383cb6
Pierre <pierre.lagadec@gmail.com>
2022-05-04 13:38:12 +1000

    loading a model using three.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+import * as THREE from 'three';
+import { DDSLoader } from '/threejs/jsm/loaders/DDSLoader.js';
+import { MTLLoader } from '/threejs/jsm/loaders/MTLLoader.js';
+import { OBJLoader } from '/threejs/jsm/loaders/OBJLoader.js';
+
 const width = 640;
 const height = 480;
 
@@ -8,20 +13,49 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
 camera.position.z = 5;
+
+var object, texture, objectLoaded = true;
+
 
 function animate() {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 };
+
+function onProgress(xhr){}
+function onError(){}
+
+var manager = new THREE.LoadingManager();
+manager.addHandler(/\.dds$/i, new DDSLoader());
+
+var light = new THREE.HemisphereLight(0xffffff, 0x000000);
+light.position.set(0, 50, 0);
+scene.add(light);
+
+new MTLLoader(manager)
+.setPath("model/")
+.load("rio.mtl", function(materials){
+
+    materials.preload();
+
+    new OBJLoader(manager)
+    .setMaterials(materials)
+    .setPath("model/")
+    .load("rio.obj", function(obj){
+
+        object = obj;
+        object.scale.x = 0.03;
+        object.scale.y = 0.03;
+        object.scale.z = 0.03;
+
+        object.position.y = -1;
+
+        scene.add(object);
+        objectLoaded = true;
+    });
+});
 
 animate();
